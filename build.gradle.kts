54e84fcb85b155fc591a763b90b70cd055da6e8c
Jasper de Vries <jasper.devries@the-future-group.com>
2022-10-20 11:22:56 +0200

    Liquibase upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,12 @@ val junitVersion = "5.9.0"
 dependencies {
     implementation("org.mybatis.generator:mybatis-generator-core:1.4.1")
 
-    compileOnly("org.liquibase:liquibase-core:4.16.1")
-    // Temporary explicit snakeyaml until liquibase releases a fixed version
-    compileOnly("org.yaml:snakeyaml:1.32")
+    compileOnly("org.liquibase:liquibase-core:4.17.0")
+    // Temporary explicit commons-text until liquibase releases a fixed version
+    compileOnly("org.apache.commons:commons-text:1.10.0")
 
     testImplementation("com.h2database:h2:2.1.214")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.4.2")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.1")
     // keep on 1.12.4 until Gradle supports Kotlin 1.7
     testImplementation("io.mockk:mockk:1.12.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
